9f849b360499fcb862c6a5bc6d5def36c4fa9111
blizary <rosajunior05@gmail.com>
2021-08-01 11:10:22 +0100

    pew pew all good now

--- Assets/Scripts/General/Shooting.cs
+++ Assets/Scripts/General/Shooting.cs
@@ -10,6 +10,7 @@ public class Shooting : MonoBehaviour
 
     public Transform fireBullet;
     public GameObject bulletsPrefab;
+    public LayerMask coneLayer;
 
     public float bulletSpeed = 20f;
 
@@ -43,15 +44,18 @@ public class Shooting : MonoBehaviour
 
         if(Input.GetButtonDown("Fire1"))
         {
-            /*RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.up, Mathf.Infinity, ConeofVision);
-            Debug.Log(hit.collider.gameObject.name);
-            if (hit.collider.gameObject.CompareTag("coneVision"))
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.up, Mathf.Infinity, coneLayer);
+            if(hit)
             {
-                Debug.Log("pew pew!");
-                Shoot();
-            }*/
+                if (hit.collider.gameObject.CompareTag("coneVision"))
+                {
+                    Debug.Log("pew pew!");
+                    Shoot(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                }
+            }
+            
             bulletText.text = "" + currentAmmo.ToString() + " / " + maxAmmo.ToString();
-            Shoot();
+            //Shoot();
         }
     }
 
@@ -64,13 +68,22 @@ public class Shooting : MonoBehaviour
         bulletText.text = "" + currentAmmo.ToString() + " / " + maxAmmo.ToString();
     }
 
-    void Shoot()
+    void Shoot(Vector3 hitPoint)
     {
         currentAmmo--;
 
-        GameObject bullet = Instantiate(bulletsPrefab, fireBullet.position, fireBullet.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(fireBullet.up * bulletSpeed, ForceMode2D.Impulse);
+        GameObject bullet = Instantiate(bulletsPrefab, fireBullet.position, fireBullet.rotation);//Spawn bullet
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();//get bullet rigidbody
+        Vector3 hitpointFix = new Vector3(hitPoint.x, hitPoint.y, fireBullet.position.z);// make sure the z is the same as plane where the bullet is spawned
+        //Debug.DrawLine(fireBullet.transform.position, hitpointFix, Color.red,100000F);
+        Vector3 dir = (hitpointFix - fireBullet.transform.position).normalized;// get the direction of movement by using the starting point and the mouse point
+        bullet.transform.up = hitpointFix - bullet.transform.position;//rotate the bullet so it faces where it is moving
+
+        rb.AddForce(dir*bulletSpeed, ForceMode2D.Impulse);//apply force to the bullet in the direction wanted
+        
+        //bullet.transform.rotation = Quaternion.LookRotation(rb.velocity);
+
+
     }
 
     // I need to make the shooting go to the mouse position 
